a8bfa530b002ca2e997b27643771849337d7dacd
aleksandra <aleksandra.ovsia@gmail.com>
2025-08-27 22:58:46 +0300

    eds

--- wp-content/themes/redtop/page.php
+++ wp-content/themes/redtop/page.php
@@ -19,42 +19,16 @@ get_header();
 <main id="primary" class="site-main">
 
 	<section class="page-section">
-		<header class="page-header">
-			<?php
-
-			if (has_post_thumbnail()) {
-				the_post_thumbnail();
-			} else {
-				echo '<img src="' . get_bloginfo("template_url") . '/images/svg/placeholder.svg" />';
-			}
-			?>
-			<div class="fixed-container">
-				<ul class="breadcrumbs__list">
-					<?php echo site_breadcrumbs(); ?>
-				</ul>
-				<?php
-				the_title('<h2 class="page-title">', '</h2>');
-				?>
-			</div>
-		</header><!-- .page-header -->
 
 		<div class="page-section__content">
 			<div class="fixed-container">
-				<?php
-				while (have_posts()) :
-					the_post();
-
-					//get_template_part('template-parts/content', 'page');
-					the_content();
-
-				endwhile; // End of the loop.
-				?>
+				<?php the_content(); ?>
 			</div>
 		</div>
 
 	</section>
 
-	<?php get_template_part('template-parts/messengers-block'); ?>
+	<?php get_template_part('template-parts/socials'); ?>
 </main><!-- #main -->
 
 <?php
